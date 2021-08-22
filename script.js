ccc4d98948e22fe1999af53071b922fdae24b91b
Carlos Rodrigues <72260079+cadu088@users.noreply.github.com>
2021-08-22 13:53:10 -0300

    Atualização total V0.1

--- script.js
+++ script.js
@@ -37,7 +37,67 @@ function pesquisar() {
         var site = "https://www.google.com/search?q=" + pesquisaTratada;
         return window.location.href = site;
     }
+}
+document.addEventListener('keydown', (e) => {
+    if (e.keyCode == 13) {
+        pesquisar();
+    }
+}, false);
+
+//modo noturno
+// const options = {
+//     bottom: '32px', // default: '32px'
+//     right: '32px', // default: '32px'
+//     left: 'unset', // default: 'unset'
+//     time: '0.3s', // default: '0.3s'
+//     mixColor: '#fff', // default: '#fff'
+//     backgroundColor: '#fff', // default: '#fff'
+//     buttonColorDark: 'rgba(68, 0, 255, 0.2)', // default: '#100f2c'
+//     buttonColorLight: '#fff', // default: '#fff'
+//     saveInCookies: true, // default: true,
+//     label: '', // default: ''
+//     autoMatchOsTheme: true, // default: true
+// }
+
+// const darkmode = new Darkmode(options);
+
+// darkmode.showWidget();
+
+//data texto 
 
 
 
+
+function relogio() {
+    const local = window.document.getElementById('dataText');
+    const texto = window.document.getElementById('DataTexto');
+    const emoji = window.document.getElementById('emojin')
+    const hora = new Date();
+    const atual = hora.getHours();
+    if (atual >= 12 && atual < 18) {
+        texto.innerText = 'Boa tarde!'
+        emoji.innerText = '🎑'
+    } else if (atual >= 18) {
+        texto.innerText = 'Boa noite!'
+        emoji.innerText = '​🌙'
+    } else if (atual >= 0 && atual < 5) {
+        texto.innerText = 'Boa noite!'
+        emoji.innerText = '🌆'
+    } else {
+        texto.innerText = 'Bom dia!'
+        emoji.innerText = '​☀'
+    }
+    var data = new Date();
+    var dia = data.getDate();
+    var mes = data.getMonth();
+    var horas = data.getHours();
+    var min = data.getMinutes();
+    var str_data = dia + '/' + (mes + 1);
+    var str_hora = horas + ':' + min;
+
+    var dias = new Array(
+        'domingo', 'segunda', 'terça', 'quarta', 'quinta', 'sexta', 'sábado'
+    );
+    dataTime.innerText = ('Hoje é ' + dias[data.getDay()] + ' dia ' + str_data + ' às ' + str_hora);
+    setTimeout("relogio()", 1000);
 }
